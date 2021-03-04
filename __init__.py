1a7701388cab60318b9119c192f0300f930c1177
jarbasal <jarbasai@mailfence.com>
2021-03-04 00:11:25 +0000

    better_cps

--- __init__.py
+++ __init__.py
@@ -1,17 +1,11 @@
-from ovos_utils.waiting_for_mycroft.common_play import CPSMatchLevel, \
-    CPSMatchType
-from ovos_utils.skills.templates.media_collection import MediaCollectionSkill
+from ovos_utils.skills.templates.video_collection import VideoCollectionSkill
 from mycroft.skills.core import intent_file_handler
-from mycroft.util.parse import fuzzy_match, match_one
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-import random
-import re
-from json_database import JsonStorageXDG
-import datetime
+from ovos_utils.playback import CPSMatchType, CPSPlayback, CPSMatchConfidence
 
 
-class DustSkill(MediaCollectionSkill):
+class DustSkill(VideoCollectionSkill):
 
     def __init__(self):
         super().__init__("Dust")
@@ -23,6 +17,10 @@ class DustSkill(MediaCollectionSkill):
         path = join(dirname(__file__), "res", "dust.jsondb")
         logo = join(dirname(__file__), "res", "dust_logo.png")
         self.media_collection = Collection("dust", logo=logo, db_path=path)
+        self.default_image = join(dirname(__file__), "ui", "dust_icon.png")
+        self.skill_logo = join(dirname(__file__), "ui", "dust_icon.png")
+        self.skill_icon = join(dirname(__file__), "ui", "dust_icon.png")
+        self.default_bg = logo
 
     # voice interaction
     def get_intro_message(self):
@@ -32,39 +30,7 @@ class DustSkill(MediaCollectionSkill):
     def handle_homescreen_utterance(self, message):
         self.handle_homescreen(message)
 
-    # matching
-    def match_media_type(self, phrase, media_type):
-        match = None
-        score = 0
-
-        if self.voc_match(phrase,
-                          "video") or media_type == CPSMatchType.VIDEO:
-            score += 0.05
-            match = CPSMatchLevel.GENERIC
-
-        if self.voc_match(phrase, "short"):
-            score += 0.15
-            match = CPSMatchLevel.CATEGORY
-
-        if self.voc_match(phrase, "scifi"):
-            score += 0.1
-            match = CPSMatchLevel.CATEGORY
-
-        if self.voc_match(phrase, "horror"):
-            score += 0.05
-            match = CPSMatchLevel.CATEGORY
-
-        if self.voc_match(phrase,
-                          "movie") or media_type == CPSMatchType.MOVIE:
-            score += 0.1
-            match = CPSMatchLevel.CATEGORY
-
-        if self.voc_match(phrase, "dust"):
-            score += 0.3
-            match = CPSMatchLevel.TITLE
-
-        return match, score
-
+    # better common play
     def normalize_title(self, title):
         title = title.lower().strip()
         title = self.remove_voc(title, "dust")
@@ -78,19 +44,31 @@ class DustSkill(MediaCollectionSkill):
             .strip()
         return " ".join([w for w in title.split(" ") if w])  # remove extra spaces
 
-    def calc_final_score(self, phrase, base_score, match_level):
-        score = base_score
-        if self.voc_match(phrase, "dust"):
-            score += 0.15
-        if self.voc_match(phrase, "short"):
-            score += 0.05
-            if self.voc_match(phrase, "movie"):
-                score += 0.05  # bonus for short films
+    def match_media_type(self, phrase, media_type):
+        score = 0
+        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
+            score += 5
 
-        # optionally return new match_level
-        # return score, match_level
+        if self.voc_match(phrase, "short"):
+            score += 5
+
+        if self.voc_match(phrase, "scifi"):
+            score += 15
+            if self.voc_match(phrase, "dust"):
+                score += 40
+
+        if self.voc_match(phrase, "horror"):
+            score += 5
+
+        if self.voc_match(phrase, "movie") or media_type == CPSMatchType.MOVIE:
+            score += 5
+
+        if self.voc_match(phrase, "dust"):
+            score += 10
+
         return score
 
 
 def create_skill():
     return DustSkill()
+
