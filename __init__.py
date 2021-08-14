18fb4e111925311d7c0b6d0afc61b271e5f4dfb2
jarbasal <jarbasai@mailfence.com>
2021-08-14 14:02:15 +0100

    .

--- __init__.py
+++ __init__.py
@@ -1,29 +1,32 @@
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
+    CommonPlayMatchConfidence
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
+import biblioteca
+
 
 class DustSkill(VideoCollectionSkill):
 
     def __init__(self):
         super().__init__("Dust")
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.MOVIE,
-                                CPSMatchType.SHORT_FILM,
-                                CPSMatchType.VIDEO]
+        self.supported_media = [CommonPlayMediaType.GENERIC,
+                                CommonPlayMediaType.MOVIE,
+                                CommonPlayMediaType.SHORT_FILM,
+                                CommonPlayMediaType.VIDEO]
         self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
         # load video catalog
-        path = join(dirname(__file__), "res", "dust.jsondb")
+        base_folder = biblioteca.download("ytcat_dust")
+        path = join(base_folder, "dust.jsondb")
         logo = join(dirname(__file__), "res", "dust_logo.png")
         self.media_collection = Collection("dust", logo=logo, db_path=path)
         self.default_image = join(dirname(__file__), "ui", "dust_icon.png")
         self.skill_logo = join(dirname(__file__), "ui", "dust_icon.png")
         self.skill_icon = join(dirname(__file__), "ui", "dust_icon.png")
         self.default_bg = logo
-        self.media_type = CPSMatchType.SHORT_FILM
-        self.playback_type = CPSPlayback.GUI
+        self.media_type = CommonPlayMediaType.SHORT_FILM
+        self.playback_type = CommonPlayPlaybackType.GUI
 
     # voice interaction
     def get_intro_message(self):
@@ -49,7 +52,7 @@ class DustSkill(VideoCollectionSkill):
 
     def match_media_type(self, phrase, media_type):
         score = 0
-        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
+        if self.voc_match(phrase, "video") or media_type == CommonPlayMediaType.VIDEO:
             score += 5
 
         if self.voc_match(phrase, "short"):
@@ -63,10 +66,10 @@ class DustSkill(VideoCollectionSkill):
         if self.voc_match(phrase, "horror"):
             score += 5
 
-        if self.voc_match(phrase, "movie") or media_type == CPSMatchType.MOVIE:
+        if self.voc_match(phrase, "movie") or media_type == CommonPlayMediaType.MOVIE:
             score += 10
 
-        if media_type == CPSMatchType.SHORT_FILM:
+        if media_type == CommonPlayMediaType.SHORT_FILM:
             score += 30
 
         if self.voc_match(phrase, "dust"):
