5343a3cc5da02cfcdcd2ffff42bb29988b360d16
jarbasai <jarbasai@mailfence.com>
2022-08-20 20:41:02 +0100

    db db db scrap scrap

--- scripts/update_jsondb.py
+++ scripts/update_jsondb.py
@@ -1,11 +1,26 @@
 from youtube_archivist import YoutubeMonitor
 import shutil
-from os.path import dirname
+import json
+from os.path import dirname, isfile
 
 url = "https://www.youtube.com/c/watchdust"
 archive = YoutubeMonitor("dust",
                          blacklisted_kwords=["trailer"])
 
+# load previous cache
+cache_file = f"{dirname(dirname(__file__))}/bootstrap.json"
+if isfile(cache_file):
+    try:
+        with open(cache_file) as f:
+            data = json.load(f)
+            archive.db.update(data)
+            archive.db.store()
+    except:
+        pass  # corrupted for some reason
+
+
+# parse new vids
 archive.parse_videos(url)
 
-shutil.copy(archive.db.path, f"{dirname(dirname(__file__))}/bootstrap.json")
+# save bootstrap cache
+shutil.copy(archive.db.path, cache_file)
